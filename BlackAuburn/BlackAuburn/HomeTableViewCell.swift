4ff9cdad1d31707349953d3923c0b370d8969be3
ALYSSA <alyssasgable@gmail.com>
2018-02-27 21:30:27 -0600

    Made space in between collection cells smaller, added drop shadow to cells

---
+++ BlackAuburn/BlackAuburn/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  BlackAuburn
+//
+//  Created by ALYSSA on 2/27/18.
+//  Copyright © 2018 AlyssaGable. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
