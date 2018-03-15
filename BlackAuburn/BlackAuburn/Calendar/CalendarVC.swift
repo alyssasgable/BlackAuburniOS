4bee67a31f936ae2b79d8b7500a198a603264baa
ALYSSA <alyssasgable@gmail.com>
2018-03-15 10:34:26 -0400

    Added calendar and added gesture recognizers to change month and month label

--- BlackAuburn/BlackAuburn/Calendar/CalendarVC.swift
+++ BlackAuburn/BlackAuburn/Calendar/CalendarVC.swift
@@ -9,34 +9,67 @@
 import UIKit
 import SnapKit
 import FSCalendar
+import Koyomi
 
-
-class CalendarVC: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+class CalendarVC: UIViewController {
     
-    fileprivate weak var calendar: FSCalendar!
+    var monthLabel = UILabel()
+    var calendar = Koyomi(frame: CGRect(x: 10, y : 20, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2), sectionSpace: 1.5, cellSpace: 0.5, inset: .zero, weekCellHeight: 25)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
-        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
-        calendar.dataSource = self
-        calendar.delegate = self
-        view.addSubview(calendar)
-        self.calendar = calendar
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        setUpView()
     }
     
-    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-        calendar.snp.updateConstraints { (make) in
-            make.edges.equalToSuperview()
-            // Do other updates
+    func setUpView() {
+        monthLabel.text = calendar.currentDateString()
+        monthLabel.font = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.light)
+        view.addSubview(monthLabel)
+        
+        calendar = Koyomi(frame: CGRect(x: 10, y : 20, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2), sectionSpace: 1.5, cellSpace: 0.5, inset: .zero, weekCellHeight: 25)
+        calendar.selectionMode = .multiple(style: .background)
+        view.addSubview(calendar)
+        
+        monthLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(75)
+            make.left.equalTo(150)
         }
-        self.view.layoutIfNeeded()
+        
+        calendar.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
+            make.height.equalTo(UIScreen.main.bounds.height/2)
+            make.top.equalTo(monthLabel.snp.bottom)
+        }
     }
+    
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                calendar.display(in: .previous)
+                monthLabel.text = calendar.currentDateString()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            case UISwipeGestureRecognizerDirection.left:
+                calendar.display(in: .next)
+                monthLabel.text = calendar.currentDateString()
+            default:
+                break
+            }
+        }
     }
+  
     
 }
